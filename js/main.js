9e57e11d8a3ad70d84d5305a5eb70091c74af11d
galla-okto <galla.okto1@gmail.com>
2019-09-22 21:13:17 +0300

    Вносит исправления по замечаниям

--- js/main.js
+++ js/main.js
@@ -16,9 +16,10 @@ var SKY_WIDTH = 170;
 var PRICE_MAX = 2000;
 var ROOMS_MAX = 5;
 var GUESTS_MAX = 8;
+var NUMBER_MAX = 9;
 
-var getRandom = function (max) {
-  return Math.round(Math.random() * max);
+var getRandom = function (max, including = 0, min = 0) {
+  return Math.round(Math.random() * (max - min - (including ? 1 : 0))) + min;
 };
 
 var userDialog = document.querySelector('.map');
@@ -28,25 +29,29 @@ var similarListElement = userDialog.querySelector('.map__pins');
 
 var similarMapInTemplate = document.querySelector('#pin').content.querySelector('button');
 
-var rentalAds = [];
+var getAvatar = function (index) {
+  return 'img/avatars/user' + (index > NUMBER_MAX ? '' : '0') + index + '.png';
+}
 
 var getRentalAds = function () {
-  for (var i = 0; i < RENTAL_ADS_QUANTITY; i++) {
+  var rentalAds = [];
+
+  for (var i = 1; i <= RENTAL_ADS_QUANTITY; i++) {
     rentalAds.push({
       'author': {
-        'avatar': 'img/avatars/user' + (i + 1 > 9 ? '' : '0') + (i + 1) + '.png'
+        'avatar': getAvatar(i)
       },
       'offer': {
-        'title': 'Apartment Nr ' + i + 1,
+        'title': 'Apartment Nr ' + i,
         'address': getRandom(MAP_WIDTH) + ', ' + getRandom(MAP_HEIGHT),
         'price': getRandom(PRICE_MAX),
-        'type': TYPE_APARTMENTS[getRandom(TYPE_APARTMENTS.length - 1)],
+        'type': TYPE_APARTMENTS[getRandom(TYPE_APARTMENTS.length, 1)],
         'rooms': getRandom(ROOMS_MAX),
         'guests': getRandom(GUESTS_MAX),
-        'chekin': CHECK_IN[getRandom(CHECK_IN.length - 1)],
-        'chekout': CHECK_OUT[getRandom(CHECK_OUT.length - 1)],
-        'features': FEATURES[getRandom(FEATURES.length - 1)],
-        'description': DESCRIPTION[getRandom(DESCRIPTION.length - 1)],
+        'chekin': CHECK_IN[getRandom(CHECK_IN.length, 1)],
+        'chekout': CHECK_OUT[getRandom(CHECK_OUT.length, 1)],
+        'features': FEATURES[getRandom(FEATURES.length, 1)],
+        'description': DESCRIPTION[getRandom(DESCRIPTION.length, 1)],
         'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
           'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
           'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
@@ -57,24 +62,36 @@ var getRentalAds = function () {
       }
     });
   }
+
+  return rentalAds;
 };
+
+var assignValuesElement = function(rentalAdsElement, pin) {
+  var location = pin.location;
+
+  rentalAdsElement.style = 'left: ' + location.x + 'px; top: ' + location.y + 'px';
+  rentalAdsElement.querySelector('img').src = pin.author.avatar;
+  rentalAdsElement.querySelector('img').alt = pin.offer.title;
+}
 
 var renderMapIn = function (pin) {
   var rentalAdsElement = similarMapInTemplate.cloneNode(true);
 
-  rentalAdsElement.style = 'left: ' + pin.location.x + 'px; top: ' + pin.location.y + 'px';
-  rentalAdsElement.querySelector('img').src = pin.author.avatar;
-  rentalAdsElement.querySelector('img').alt = pin.offer.title;
+  assignValuesElement(rentalAdsElement, pin);
 
   return rentalAdsElement;
 };
 
-getRentalAds();
+var showRentalAds = function () {
+  var fragment = document.createDocumentFragment();
 
-var fragment = document.createDocumentFragment();
+  rentalAds.forEach(function (element) {
+    fragment.appendChild(renderMapIn(element));
+  });
 
-rentalAds.forEach(function (element) {
-  fragment.appendChild(renderMapIn(element));
-});
+  similarListElement.appendChild(fragment);
+}
 
-similarListElement.appendChild(fragment);
+var rentalAds = getRentalAds();
+
+showRentalAds();
